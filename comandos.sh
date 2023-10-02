f253a515da843d70d02a822367c0c9598e860438
Valentina Fabris <129615472+vfabris@users.noreply.github.com>
2023-10-02 12:36:49 -0300

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -1,5 +1,10 @@
+sed 's/\./\.\n/g' breve_historia.txt | sed '/^[[:space:]]*$/d' > breve_historia_2.txt
+sed 's/\./\.\n/g' breve_historia.txt | grep 'independencia' -i
+grep '^El[^.]*\.' breve_historia.txt
+sed 's/\./\.\n/g' breve_historia.txt | grep 'peronismo' -c
 sed 's/\./\.\n/g' breve_historia.txt | grep 'Sarmiento' | grep 'Rosas' -c
+sed 's/\./\.\n/g' breve_historia.txt | grep -E '(180[1-9]|18[1-9][0-9]|1900)' >respuestasPuntoH.txt
 
 ls | grep '[^.]*\.txt'| nl
 
-sed 's/\./\.\n/g' breve_historia.txt | grep -E '(180[1-9]|18[1-9][0-9]|1900)' >respuestasPuntoH.txt
+
